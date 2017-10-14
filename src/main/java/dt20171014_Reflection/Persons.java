8bdc05ff179f849db63e950dc17f546dfda199ef
Zak <zak@Nasirs-MBP.fios-router.home>
2017-10-14 10:50:50 -0400

    reflection practice

---
+++ src/main/java/dt20171014_Reflection/Persons.java
@@ -0,0 +1,10 @@
+package dt20171014_Reflection;
+
+public class Persons {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+}
---
+++ src/main/java/dt20171014_Reflection/Reflection_driver.java
@@ -0,0 +1,9 @@
+package dt20171014_Reflection;
+
+public class Reflection_driver {
+    public static void main(String[] args) {
+        Persons p = new Persons();
+        System.out.println(p.getName());
+    }
+
+}
